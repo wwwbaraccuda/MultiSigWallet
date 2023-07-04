bf80e2fdb58428eafec3dd353ecd3352f82970e7
Ardian <vincentiusardian@gmail.com>
2023-07-04 16:15:22 +0700

    Update MultiSigWalletV1.sol

--- MultiSigWalletV1.sol
+++ MultiSigWalletV1.sol
@@ -5,24 +5,27 @@ pragma solidity ^0.8.9;
 contract MultiSigWallet {
 
     address[] owners;
-    uint totalrequiredsignature;
+    uint totalRequiredConfimation;
 
     struct Transaction {
         address to;
         uint value;
         bytes data;
         bool executed;
-        mapping(address => bool) signatures;
     }
 
+    mapping(uint=>mapping(address=>bool)) isConfirmed;    
     Transaction[] private transactions;
     
-    constructor(address[] memory _owners, uint _totalrequiredsignature) {
-        require(_owners.length > 0, "Input owner");
-        require(_totalrequiredsignature == _owners.length, "Signature Kurang" );
+    event TransactionSubmitted(uint txIndex, address sender, address receiver, uint amount);
+    event TransactionSigned(uint txIndex);
+    event TransactionExecuted(uint txIndex);
 
+    constructor(address[] memory _owners, uint _totalRequiredConfimation) {
+        require(_owners.length > 1, "Input owner");
+        require(_totalRequiredConfimation == _owners.length, "Signature Kurang" );
         owners = _owners;
-        totalrequiredsignature = _totalrequiredsignature;
+        totalRequiredConfimation = _totalRequiredConfimation;
     }
 
     modifier ownerOnly() {
@@ -37,16 +40,16 @@ contract MultiSigWallet {
         _;
     }
 
-    function submitTransaction(address _to, uint _value, bytes memory _data) public {
+    function submitTransaction(address _to, bytes memory _data) public ownerOnly payable{
+        require(_to!=address(0), "Invalid Receiver's Address");
+        require(msg.value>0, "Input amount");
         uint txIndex = transactions.length;
+        transactions.push(Transaction({to: _to, value: msg.value, data: _data, executed: false}));
+        emit TransactionSubmitted(txIndex, msg.sender, _to, msg.value);
+    }
 
-        transactions.push();
-
-        Transaction storage transaction = transactions[txIndex];
-        transaction.to = _to;
-        transaction.value = _value;
-        transaction.data = _data;
-        transaction.executed = false;
+    function getOwners() public view returns(address[] memory) {
+        return owners;
     }
 
     function getTransaction(uint txIndex) public ownerOnly view returns(address _to, uint _value, bytes memory _data) {
@@ -54,30 +57,31 @@ contract MultiSigWallet {
         return(transaction.to, transaction.value, transaction.data);
     }
 
-    function signTransaction(uint txIndex) public {
-        require(txIndex == transactions.length, "Invalid number");
-        require(!transactions[txIndex].signatures[msg.sender], "Already signed");
-
-        transactions[txIndex].signatures[msg.sender] = true;
+    function confirmTransaction(uint txIndex) public ownerOnly {
+        require(txIndex < transactions.length, "Invalid number");
+        require(!isConfirmed[txIndex][msg.sender], "Already confirmed");
+        isConfirmed[txIndex][msg.sender] = true;
+        emit TransactionSigned(txIndex);
     }
 
-    function getNumSignatures(Transaction storage _transaction) private view returns (uint) {
-        uint count = 0;
+    function isTransactionConfirmed(uint txIndex) public view returns (bool) {
+        require(txIndex < transactions.length, "Invalid number");
+        uint count=0;
+
         for (uint i = 0; i < owners.length; i++) {
-            if (_transaction.signatures[owners[i]]) {
+            if (isConfirmed[txIndex][owners[i]]) {
                 count++;
             }
         }
-        return count;
+        return count>=totalRequiredConfimation;
     }
 
-    function executeTransaction(uint txIndex) public {
-        require(txIndex == transactions.length, "Invalid number");
+    function executeTransaction(uint txIndex) public payable ownerOnly {
+        require(txIndex < transactions.length, "Invalid number");
         require(!transactions[txIndex].executed, "Already executed");
-
-        Transaction storage transaction = transactions[txIndex];
-        require(getNumSignatures(transaction) == totalrequiredsignature, "Not enough sign");
-
-        transaction.executed = true;
+        (bool success,) = transactions[txIndex].to.call{value: transactions[txIndex].value}("");
+        require(success, "Transaction Failed");
+        transactions[txIndex].executed = true;
+        emit TransactionExecuted(txIndex);
     }
 }
